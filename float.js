b2a1c4ec31153a7e6127581f52ecca352bd9e708
nossika <476492581@qq.com>
2017-01-17 10:40:27 +0800

    improve code style

--- float.js
+++ float.js
@@ -1,6 +1,6 @@
 (factory => {
-    let root = (typeof self == 'object' && self.self === self && self) ||
-        (typeof global == 'object' && global.global === global && global);
+    let root = (typeof self === 'object' && self.self === self && self) ||
+        (typeof global === 'object' && global.global === global && global);
     if (typeof define === 'function' && define.amd) {
         define([], ()=> {
             root.Float = factory();
@@ -11,6 +11,7 @@
         root.Float = factory();
     }
 })(() => {
+
     let _config = Object.seal({
         dot_link: 100,
         dot_v: [1, 6],
@@ -27,17 +28,26 @@
             dot: {r: 255, g: 255, b: 255, a: 1},
         })
     });
+
     let _style = _config.style;
     let _ctx, _ctx_w, _ctx_h, _mouse_dot, _dot_list, _animating_id, _borders;
+
     class Dot {
-        constructor(config){
-            this.x = config.x || 0;
-            this.y = config.y || 0;
-            this.v_x = config.v_x || 0;
-            this.v_y = config.v_y || 0;
-            this.r = config.r || 0;
+        constructor({
+            x = 0,
+            y = 0,
+            v_x = 0,
+            v_y = 0,
+            r = 0
+        }){
+            this.x = x;
+            this.y = y;
+            this.v_x = v_x;
+            this.v_y = v_y;
+            this.r = r;
         }
     }
+
     let json_clone = (target, source) => { // cover target with source
         for (let item in source) {
             if (source[item] instanceof Object) {
@@ -45,17 +55,20 @@
                     ? target[item]
                     : (Object.prototype.toString.call(source[item]) === '[object Array]' ? [] : {});
                 json_clone(target[item], source[item]);
-            }else {
+            } else {
                 target[item] = source[item];
             }
         }
     };
+
     let get_num = (source, fixed = 1) => { // source could be Number or Array
         return source instanceof Array
             ? source[0] + Math.floor(Math.random() * ((source[1] - source[0]) * fixed + 1)) / fixed
             : source;
     };
+
     let Float = {
+
         init (canvas, config = {}) {
             _ctx = canvas.getContext('2d');
             _mouse_dot = null;
@@ -67,18 +80,20 @@
             Float.suspend();
             Float.resume();
         },
+
         bind_event () {
             let events = [];
-            if(_config.on_click) events.push('click');
-            if(_config.on_mousemove) events.push('mousemove', 'mouseleave');
+            if (_config.on_click) events.push('click');
+            if (_config.on_mousemove) events.push('mousemove', 'mouseleave');
             events.forEach((event) => {
                 _ctx.canvas.removeEventListener(event, Float[`on_${event}`]);
                 _ctx.canvas.addEventListener(event, Float[`on_${event}`]);
             });
         },
+
         on_mousemove (e) {
             let [x, y] = [e.offsetX, e.offsetY];
-            if(!_mouse_dot){
+            if (!_mouse_dot) {
                 _mouse_dot = new Dot({x: x, y: y});
                 _dot_list.add(_mouse_dot);
                 return;
@@ -86,50 +101,56 @@
             _mouse_dot.x = x;
             _mouse_dot.y = y;
         },
+
         on_mouseleave (e) {
             _dot_list.delete(_mouse_dot);
             _mouse_dot = null;
         },
+
         on_click (e) {
             let [x, y] = [e.offsetX, e.offsetY];
             let n = get_num(_config.dot_click);
-            for(let i = 0; i < n; i++) {
+            for (let i = 0; i < n; i++) {
                 _dot_list.add(new Dot({
                     x: x,
                     y: y,
                     v_x: get_num(_config.dot_v, 100) * (Math.random() > 0.5 ? -1 : 1) / 10,
                     v_y:get_num(_config.dot_v, 100) * (Math.random() > 0.5 ? -1 : 1) / 10,
-                    r: get_num(_config.dot_r, 100)
+                    r: get_num(_config.dot_r, 100),
                 }))
             }
         },
+
         resume () {
             cancelAnimationFrame(_animating_id);
             let animation = () => {
                 Float.clear();
                 Float.render();
-                _animating_id = requestAnimationFrame(animation)
+                _animating_id = requestAnimationFrame(animation);
             };
-            _animating_id = requestAnimationFrame(animation)
+            _animating_id = requestAnimationFrame(animation);
         },
+
         suspend () {
             cancelAnimationFrame(_animating_id);
         },
+
         update (dot) {
             let [top, right, bottom, left] = _borders;
             dot.x = dot.x + dot.v_x;
             dot.y = dot.y + dot.v_y;
-            if(dot.x < left || dot.x > right || dot.y < top || dot.y > bottom){
+            if (dot.x < left || dot.x > right || dot.y < top || dot.y > bottom) {
                 _dot_list.delete(dot);
-                if(_dot_list.size > _config.dot_max) return;
+                if (_dot_list.size > _config.dot_max) return;
                 Float.add_dot();
             }
         },
+
         render () {
             let temp_list = new Set();
             _dot_list.forEach((dot) => {
                 Float.update(dot);
-                if(!_dot_list.has(dot)) return;
+                if (!_dot_list.has(dot)) return;
                 _ctx.beginPath();
                 _ctx.moveTo(dot.x, dot.y);
                 _ctx.arc(dot.x, dot.y, dot.r, 0, Math.PI * 2);
@@ -137,8 +158,8 @@
                 _ctx.fillStyle = `rgba(${_style.dot.r},${_style.dot.g},${_style.dot.b},${1})`;
                 _ctx.fill();
                 temp_list.forEach((other_dot) => {
-                    let dis = Math.pow(Math.pow(dot.x - other_dot.x, 2) + Math.pow(dot.y - other_dot.y, 2), 1/2)
-                    if(dis > _config.dot_link) return;
+                    let dis = Math.pow(Math.pow(dot.x - other_dot.x, 2) + Math.pow(dot.y - other_dot.y, 2), 1/2);
+                    if (dis > _config.dot_link) return;
                     _ctx.beginPath();
                     _ctx.moveTo(dot.x, dot.y);
                     _ctx.lineTo(other_dot.x, other_dot.y);
@@ -150,15 +171,17 @@
                 temp_list.add(dot);
             });
         },
+
         auto_add_dot (num) {
             let interval = setInterval(() => {
-                if(_dot_list.size > num) {
+                if (_dot_list.size > num) {
                     clearInterval(interval);
                     return;
                 }
                 Float.add_dot();
-            }, _config.dot_create)
+            }, _config.dot_create);
         },
+
         add_dot () {
             let [x, y, v_x, v_y] = [
                 get_num([0, _ctx_w]),
@@ -193,14 +216,16 @@
                 r: get_num(_config.dot_r, 100)
             }));
         },
+
         set_style (style) {
             style = JSON.parse(JSON.stringify(style));
-            for(let prop in style){
+            for (let prop in style) {
                 _style[prop] = style[prop];
             }
         },
+
         set_size () {
-            if(!_ctx) return;
+            if (!_ctx) return;
             _ctx_w = _ctx.canvas.offsetWidth;
             _ctx_h = _ctx.canvas.offsetHeight;
             _ctx.canvas.width = _ctx_w;
@@ -212,11 +237,15 @@
                 0 - _config.extend_border,
             ];
         },
+
         clear () {
             _ctx.clearRect(0, 0, _ctx_w, _ctx_h);
         },
+
     };
+
     return Float;
+
 });
 
 
